99f7c85d49fb5d803cadc0b40098f85414e6776c
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-02 17:22:08 +0200

    add user with post method

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 const express = require("express");
+const uid = require("uid");
 
-//new instance of express server
 const app = express();
+app.use(express.json());
 
 let i = 0;
 
-//@param -> req = request, res = response
+let users = [
+  { name: "Joe", role: "mechanic", id: uid() },
+  { name: "Dr. Who", role: "time traveller", id: uid() },
+  { name: "Jan", role: "assistant coach", id: uid() },
+  { name: "Dalia", role: "boss", id: uid() }
+];
+
 app.get("/", (req, res) => {
   res.send(`Hello world ${i++}`);
 });
 
-//init port
-app.listen(3000, err => {
-  err ? console.log(err) : console.log("server ready...");
+app.get("/users", (req, res) => {
+  res.json(users);
 });
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.json(users.find(user => user.id === id));
+});
+
+//new user
+app.post("/users", (req, res) => {
+  let newUser = req.body;
+  newUser.id = uid();
+  console.log(newUser);
+  users = [...users, newUser];
+  res.json(newUser);
+});
+
+app.listen(3000, err => {
+  err ? console.log(err) : console.log("Server ready");
+});
+
+console.log(users);
